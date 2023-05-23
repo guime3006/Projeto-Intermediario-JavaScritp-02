6d4449b0f2a2b0949e56a5cf825c816a4e5aeec8
Guilherme Verissimo <guime.verissimo456@gmail.com>
2023-05-23 08:00:35 -0300

    Scritp Finalizado, CSS atualizado com um Background Novo

--- scritp.js
+++ scritp.js
@@ -1,3 +1,11 @@
-let userName = prompt(`Digite Seu Nome Completo: `);
+let userNumber = prompt(`Digite um Número: `); // v(userNumber) irar receber o  1º número do user
+userNumber = Number(userNumber); // aqui ele será convertida para um Number()
 
-let recebeNome = document.getElementById("user-name").innerHTML = `Nome: ${userName}`;
+
+let userNumberNew = prompt(`Digite um Outro Número: `); // o mesmo processo é feito novamente mas agora com um novo número
+userNumberNew = Number(userNumberNew);
+
+let calculo = userNumber + userNumberNew; // é criado uma variavel que irar fazer o calculo e armazenar o resultado
+
+let resposta = document.getElementById("resp-number").innerHTML = `O Valor Somado de ${userNumber} + ${userNumberNew} = ${calculo}`;
+// por ultimo uma variavel que fica responsavel por manipular o DOM e exibir a resposta 
